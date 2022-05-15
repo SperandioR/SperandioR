35eabac4bdad354cb366f60957ff7fd4849a9e21
Rodrigo Sperandio <rodrigopaulino13@gmail.com>
2022-05-15 17:18:49 -0300

    Estudos de gráficos em linguagem python

--- SI Python/tempCodeRunnerFile.py
+++ SI Python/tempCodeRunnerFile.py
@@ -1,1 +1,6 @@
-print("Hello, Word!")
+import matplotlib.pyplot as plt
+
+notas = pd.read_csv("ratings.csv")
+notas.columns = ["usuarioID", "filmeID", "nota", "momento"]
+notas.nota.plot(kind = 'hist')
+plt.show()
